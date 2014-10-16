45659e466cae15215a009d0d87555e8caa6bb91f
chenrujia <chenrj90@126.com>
2014-10-16 20:37:14 +0800

    第一章代码第一部分

---
+++ chapter01/FlyNoWay.java
@@ -0,0 +1,10 @@
+package chapter01;
+
+public class FlyNoWay implements FlyBehavior {
+
+	@Override
+	public void fly() {
+		System.out.println("I can't fly");
+	}
+
+}
---
+++ chapter01/MallardDuck.java
@@ -0,0 +1,15 @@
+package chapter01;
+
+public class MallardDuck extends Duck{
+	
+	public MallardDuck() {
+		quackBehavior = new Quack();
+		flyBehavior = new FlyWithWings();
+	}
+
+	@Override
+	public void display() {
+		System.out.println("I'm a real Mallard duck");
+	}
+
+}
